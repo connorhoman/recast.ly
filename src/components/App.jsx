c2cf6e8a2d7e9dd8763335ad8badba8064e9e53e
connorhoman <connorhoman14@gmail.com>
2019-04-22 19:44:12 -0700

    ESLinted

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,33 +2,34 @@ import exampleVideoData from '/src/data/exampleVideoData.js';
 import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
 
-  class App extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        videoInPlayer: exampleVideoData[0],
-        videoList: exampleVideoData.slice(1)
-      };
-    } 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videoInPlayer: exampleVideoData[0],
+      videoList: exampleVideoData.slice(1)
+    };
+  } 
 
-    render() {
-      return (<div>
-        <nav className="navbar">
-          <div className="col-md-6 offset-md-3">
-            <div><h5><em>search</em> view goes here</h5></div>
-          </div>
-        </nav>
-        <div className="row">
-          <div className="col-md-7">
-            <VideoPlayer video={this.state.videoInPlayer}/>
-          </div>
-          <div className="col-md-5">
-            <VideoList videos={this.state.videoList}/>
-          </div>
+  render() {
+    return (<div>
+      <nav className="navbar">
+        <div className="col-md-6 offset-md-3">
+          <div><h5><em>search</em> view goes here</h5></div>
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.videoInPlayer}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={this.state.videoList}/>
         </div>
       </div>
-      )}
-  };
+    </div>
+    );
+  }
+}
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
